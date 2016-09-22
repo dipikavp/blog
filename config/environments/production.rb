6071bf330e5713b1fd1e0cd00cece134099d08f6
Dipika Villiapakam Premsagar <dipika.vp@gmail.com>
2016-09-22 05:08:58 -0700

    fixed production.rb error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
     :domain               =>'heroku.com',
     :enable_starttls_auto => true
   }
-  end
+  
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
